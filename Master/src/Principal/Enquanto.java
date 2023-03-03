6f0309d0035e4afdb75220bc67f229440cd392a6
Adrian-JQuindeler <AdrianQuindeler@Gmail.com>
2023-03-03 10:49:45 -0300

    Master

--- Master/src/Principal/Enquanto.java
+++ Master/src/Principal/Enquanto.java
@@ -6,19 +6,19 @@ public class Enquanto {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner Pi = new Scanner(System.in);
 		
-		int x = sc.nextInt();
+		int x = Pi.nextInt();
 		int y = 0;
 		int soma = 0;
 		while (x != 0) {
 			y += 1;
 			soma += x;
-			x = sc.nextInt();
+			x = Pi.nextInt();
 		}
 		System.out.printf("Você digitou %d números \n", y);
 		System.out.printf("a soma de todos os números digitados é %d\n", soma);
-		sc.close();
+		Pi.close();
 
 	}
 
--- Master/src/Principal/Para.java
+++ Master/src/Principal/Para.java
@@ -6,15 +6,15 @@ public class Para{
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
+		Scanner beta = new Scanner(System.in);
 		System.out.printf("Quantos valores quer digitar?");
-		int n = sc.nextInt();
+		int n = beta.nextInt();
+		
 		int soma = 0;
 		
 		for (int i=0; i<n; i++) {
 			System.out.printf("Digite o %d valor:",i+1);
-			int x = sc.nextInt();
+			int x = beta.nextInt();
 			soma += x;}
 		System.out.printf("A soma dos valores é %d", soma);
 	}
