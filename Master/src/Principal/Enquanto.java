98a92e13d44adfeea0b1b3f8ab3fe514fea05cd6
Adrian-JQuindeler <AdrianQuindeler@Gmail.com>
2023-03-02 23:32:17 -0300

    Master

---
+++ Master/src/Principal/Enquanto.java
@@ -0,0 +1,25 @@
+package Principal;
+
+import java.util.Scanner;
+
+public class Enquanto {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = 0;
+		int soma = 0;
+		while (x != 0) {
+			y += 1;
+			soma += x;
+			x = sc.nextInt();
+		}
+		System.out.printf("Você digitou %d números \n", y);
+		System.out.printf("a soma de todos os números digitados é %d\n", soma);
+		sc.close();
+
+	}
+
+}
