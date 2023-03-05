534f4961e64d05f8fefa23ce9635925aa75c5bed
Adrian-JQuindeler <AdrianQuindeler@Gmail.com>
2023-03-05 17:36:19 -0300

    Jazz

--- Exercícios/src/Principal/Banco.java
+++ Exercícios/src/Principal/Banco.java
@@ -6,16 +6,22 @@ import Entidades.ContaDeBanco;
 public class Banco {
 
 	public static void main(String[] args) {
+		
 		/*lambda é uma letra grega*/
 		Scanner Lambda = new Scanner(System.in);
+		
 		System.out.println("CRIAR CONTA DE BANCO!");
 		System.out.println("Para criar sua conta neste banco, primeiro digite:");
+		
 		System.out.printf("Seu nome:");
 		String nome = Lambda.nextLine();
+		
 		System.out.printf("\nSeu código:");
 		int codigo = Lambda.nextInt();
+		
 		System.out.printf("\nValor inicial [Opcional] :");
 		Double valor = Lambda.nextDouble();
+		
 		ContaDeBanco conta = new ContaDeBanco(nome, codigo, valor);
 		
 		System.out.println(conta.toString());
@@ -38,6 +44,7 @@ public class Banco {
 			if (valorDeRetirada > conta.getSaldo()) {
 				System.out.println("O valor inserido é maior que o saldo disponível");
 			}
+			
 			else {
 			conta.retirar(valorDeRetirada);
 			System.out.println(conta.toString());
